fabf500e19db4a5902065c853cfa01e33fc7004e
Mario <49538587+MLSanhueza98@users.noreply.github.com>
2023-06-29 11:42:47 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+//Enlace para conectar a la base de datos, para este caso, preferimos utilizar el servicio de MongoDB Atlas que otorga un cluster en la nube totalmente gratis.
+
 mongoose.connect('mongodb+srv://API-Rest:fAWlliJQviOsuj3k@cluster0.5dbiopl.mongodb.net/Players?retryWrites=true&w=majority')
   .then(() => console.log('Conexión exitosa con la DB'))
   .catch(e => console.error('Error al conectar con la DB:', e));
